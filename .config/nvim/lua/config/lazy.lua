f933e8dcda29c6ec3e13d880a005aede655f5471
Bohdan Hrotovytskyy <bohdanhrotovytskyy@gmail.com>
2024-02-29 15:29:18 -0500

    added dimming to eyeliner and adder Ctr-[ mapping for escape

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -62,7 +62,7 @@ require("lazy").setup({
         config = function()
             require'eyeliner'.setup {
                 highlight_on_key = true, -- show highlights only after keypress
-                dim = false              -- dim all other characters if set to true (recommended!)
+                dim = true              -- dim all other characters if set to true (recommended!)
             }
         end
     },
